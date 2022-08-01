a9da49ca4270ddfe670c4ac7eea1187d0b34e151
Joseph Han <josephkhhan@gmail.com>
2022-08-01 12:06:16 -0400

    minor

--- server/db.js
+++ server/db.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 mongoose
-  .connect(`mongodb://127.0.0.1:27017/${process.env.DB_NAME}`)
+  .connect(`${process.env.DB_HOST}/${process.env.DB_NAME}`)
   .then(console.log('AHA MONGO MONGO'))
   .catch((err) => console.log(err, 'error connecting to db'));
 
